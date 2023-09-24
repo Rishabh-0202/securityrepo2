a3736ecce7f5f071ebc46c8682c6ce81b1dfadd6
Rishabh <rishabhtripathi0202@gmail.com>
2023-09-24 22:52:41 +0530

    first commit

---
+++ src/main/java/com/example/ss_2023_c3/Ss2023C3Application.java
@@ -0,0 +1,13 @@
+package com.example.ss_2023_c3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2023C3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss2023C3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ss_2023_c3/Ss2023C3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_2023_c3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2023C3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
