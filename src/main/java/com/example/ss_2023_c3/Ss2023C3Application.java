057cb5c7850309b88361bbcff6698c69078519b3
Rishabh <rishabhtripathi0202@gmail.com>
2023-09-25 22:41:57 +0530

    second

--- src/main/java/com/example/ss_2023_c3/Ss2023C3Application.java
+++ src/main/java/com/example/ss_2023_c3/Ss2023C3Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ss2023C3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ss2023C3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ss2023C3Application.class, args);
+    }
 
 }
